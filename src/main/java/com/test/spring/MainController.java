50007f86f8503e9cefb2ed51526adfa388ecbe96
sunna920619 <32083644+sunna920619@users.noreply.github.com>
2017-11-03 18:46:55 +0900

    메인컨트롤러주석

--- src/main/java/com/test/spring/MainController.java
+++ src/main/java/com/test/spring/MainController.java
@@ -7,26 +7,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 	
+	//비회원, 회원 메인
 	@RequestMapping(method = {RequestMethod.GET}, value="/main.action")
 	public String main() {
 		return "main.member";
 	}
 	
+	//매니저 메인
 	@RequestMapping(method = {RequestMethod.GET}, value="/manager/main.action")
 	public String managermain() {
 		return "main.manager";
 	}
 	
+	//학생 메인
 	@RequestMapping(method = {RequestMethod.GET}, value="/student/main.action")
 	public String studentmain() {
 		return "main.student";
 	}
 	
+	//관리자 메인
 	@RequestMapping(method = {RequestMethod.GET}, value="/admin/main.action")
 	public String adminmain() {
 		return "main.manager";
 	}
 	
+	//예제 블랭크 파일
 	@RequestMapping(method = {RequestMethod.GET}, value="/layout.action")
 	public String layout() {
 		return "layout.member";
