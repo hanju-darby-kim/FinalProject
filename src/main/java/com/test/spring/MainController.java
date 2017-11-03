bbd0a3031ea267025b8470432d48e997348ebc04
sunna920619 <32083644+sunna920619@users.noreply.github.com>
2017-11-03 17:41:13 +0900

    타일즈

--- src/main/java/com/test/spring/MainController.java
+++ src/main/java/com/test/spring/MainController.java
@@ -6,9 +6,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
+	
+	@RequestMapping(method = {RequestMethod.GET}, value="/main.action")
+	public String main() {
+		return "main.main";
+	}
+	
+	@RequestMapping(method = {RequestMethod.GET}, value="/manager/main.action")
+	public String managermain() {
+		return "main.manager";
+	}
+	
+	@RequestMapping(method = {RequestMethod.GET}, value="/student/main.action")
+	public String studentmain() {
+		return "main.student";
+	}
+	
+	@RequestMapping(method = {RequestMethod.GET}, value="/admin/main.action")
+	public String adminmain() {
+		return "main.manager";
+	}
+	
+	@RequestMapping(method = {RequestMethod.GET}, value="/layout.action")
+	public String layout() {
+		return "layout/layout";
+	}
+	
 
 	@RequestMapping(method = {RequestMethod.GET}, value="/login.action")
 	public String login() {
 		return "login";
 	}
+	
+	
 }
--- src/main/webapp/WEB-INF/views/student/studentheader.jsp
+++ src/main/webapp/WEB-INF/views/student/studentheader.jsp
@@ -229,6 +229,15 @@
 					<li><a href="blog-single.html">Blog Single</a></li>
 					<li><a href="blog-single-full.html">Blog Single Full</a></li>
 				</ul></li>
+			<li class="dropdown "><a href="#" class="dropdown-toggle"
+				data-toggle="dropdown">학생메뉴 <i class="fa fa-angle-down"></i></a>
+				<ul class="dropdown-menu" role="menu">
+					<li><a href="blog-medium-thumbnail.html">Blog Medium
+							Thumbnail</a></li>
+					<li><a href="blog-full-thumbnail.html">Blog Full Thumbnail</a></li>
+					<li><a href="blog-single.html">Blog Single</a></li>
+					<li><a href="blog-single-full.html">Blog Single Full</a></li>
+				</ul></li>
 		</ul>
 	</div>
 	<!-- END MAIN NAVIGATION -->
