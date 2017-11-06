03d8ba387cb467f1b7e8d61b2729b2a565d70dc4
sunna920619 <32083644+sunna920619@users.noreply.github.com>
2017-11-06 11:29:08 +0900

    login

--- src/main/java/com/test/spring/main/MainDAO.java
+++ src/main/java/com/test/spring/main/MainDAO.java
@@ -14,9 +14,23 @@ public class MainDAO {
 	@Autowired
 	private SqlSessionTemplate sql;
 	
-	public CertificationDTO loginok(HashMap<String, String> loginMap) {
+	/**
+	 * 입력한 id와 pw에 맞는 회원이 몇명인지 알아내는 메소드
+	 * @param loginMap
+	 * @return int 회원 수
+	 */
+	public int loginok(HashMap<String, String> loginMap) {
 		
 		return sql.selectOne("main.loginok", loginMap);
+	}
+
+	/**
+	 * 회원의 인증티켓을 얻어오는 메소드
+	 * @param loginMap
+	 * @return CertificationDTO 회원의 정보
+	 */
+	public CertificationDTO getCertification(HashMap<String, String> loginMap) {
+		return sql.selectOne("main.getCertification", loginMap);
 	}
 	
 }
