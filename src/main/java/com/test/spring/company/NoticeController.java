be22d48e74857e035488fc879e9dcd0b6f4404e1
sunna920619 <32083644+sunna920619@users.noreply.github.com>
2017-11-06 17:33:46 +0900

    notice 만드는 중

--- src/main/java/com/test/spring/company/NoticeController.java
+++ src/main/java/com/test/spring/company/NoticeController.java
@@ -1,11 +1,14 @@
 package com.test.spring.company;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class NoticeController {
 
+	@RequestMapping(method = {RequestMethod.GET}, value="/addNotice.action")
 	public String addNotice() {
 		
 		return "company.notice.addnotice";
