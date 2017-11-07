810f62825645b78ea860fcae991e28e04d51d085
babmerger <alwayswp@naver.com>
2017-11-07 11:45:36 +0900

    재준: 클래스 생성

---
+++ src/main/java/com/test/spring/jaejun/JJCurriController.java
@@ -0,0 +1,11 @@
+package com.test.spring.jaejun;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class JJCurriController {
+	
+	
+	
+	
+}
---
+++ src/main/java/com/test/spring/jaejun/JJCurriDAO.java
@@ -0,0 +1,5 @@
+package com.test.spring.jaejun;
+
+public class JJCurriDAO {
+	
+}
