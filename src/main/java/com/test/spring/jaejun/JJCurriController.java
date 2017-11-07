c7658795ce41919775f420da2eee391296c5da2b
babmerger <alwayswp@naver.com>
2017-11-07 11:54:07 +0900

    재준: DTO
    3333

--- src/main/java/com/test/spring/jaejun/JJCurriController.java
+++ src/main/java/com/test/spring/jaejun/JJCurriController.java
@@ -1,11 +1,21 @@
 package com.test.spring.jaejun;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class JJCurriController {
+	@Autowired
+	IJJCurriService service;
 	
-	
+	@RequestMapping(method={RequestMethod.GET}, value="/jaejun/add.action")
+	public String add(){
+		
+		return "add.action";
+		
+	}
 	
 	
 }
